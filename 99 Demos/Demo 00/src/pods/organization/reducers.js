7ce9ea230319ec60c049f2bc923390e3ce8230e5
Braulio <braulio.diez@gmail.com>
2017-09-26 16:38:53 +0200

    small update on reducer

--- 99 Demos/Demo 00/src/pods/organization/reducers.js
+++ 99 Demos/Demo 00/src/pods/organization/reducers.js
@@ -4,8 +4,6 @@ export const organizationReducer = (state = [], action) => {
   switch(action.type) {
       case actionsDefs.LOAD_MEMBERS_REQUEST_COMPLETED:
         return action.payload;
-      break;
   }
-
   return state;
 }
